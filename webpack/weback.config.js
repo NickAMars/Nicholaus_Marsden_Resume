ebafc0bc216e423e90316dc99014927532a4c482
NickAMars <remember.marsman@gmail.com>
2023-11-20 06:34:38 -0500

    includeing all the types needed for typescript

--- webpack/weback.config.js
+++ webpack/weback.config.js
@@ -33,7 +33,7 @@ module.exports = {
             use: ["style-loader", "css-loader", "sass-loader"],
           },
           {
-            test: /\.(tsx|js|ts)$/,
+            test: /\.(ts|js)x?$/,
             include: path.resolve(__dirname, "..", "src"),
             exclude: path.resolve(__dirname, "..", "node_modules"),
             use: "babel-loader"
